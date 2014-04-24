526c0c3bd9e74e62cd50178e02ccadfe6c39995e
nx111 <gd.zhangdz@gmail.com>
2014-04-24 14:13:18 +0800

    cpufreq: replace smartass2 governor with smartassH3
    Change-Id: If1af00971b8966baa4236a8f40ab88dcb4537ca3

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -382,6 +382,9 @@ extern struct cpufreq_governor cpufreq_gov_nightmare;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSH3)
+extern struct cpufreq_governor cpufreq_gov_smartass_h3;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass_h3)
 #endif
 
 
